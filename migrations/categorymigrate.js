6ba4c22a69b8e81481c9396d59efcdf281ad0d75
narasingam <narasinga.perumal@systimanx.com>
2023-01-21 12:41:18 +0530

    asdsad

--- migrations/categorymigrate.js
+++ migrations/categorymigrate.js
@@ -3,7 +3,7 @@ exports.up = function () {
   return knex.schema.createTable("categories", function (table) {
     table.uuid("id").primary();
     table.string("category_name").notNullable();
-    table.string("image").notNullable();
+    // table.string("image").notNullable();
   });
 };
 
